d72cd7f57c9938263e77ef844de83eaedf999e2d
tinyakov.ru <a@tinyakov.ru>
2021-01-28 16:48:44 +0300

    init commit

---
+++ src/main/java/ru/tinyakov/picnet/OtusSpringPicnetApplication.java
@@ -0,0 +1,13 @@
+package ru.tinyakov.picnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusSpringPicnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtusSpringPicnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/tinyakov/picnet/OtusSpringPicnetApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tinyakov.picnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusSpringPicnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
